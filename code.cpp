83f30a73a6ac14570a978e6bb76649ee82690801
animannaskar <143376315+animannaskar@users.noreply.github.com>
2024-03-07 12:30:54 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -2,7 +2,34 @@
 using namespace std;
 bool error=false;
 int pc = 0;
-unordered_map<string,string>  mp={{"sp","x2"},{"gp","x3"},{"tp","x4"},{"ra","x1"}};
+unordered_map<string, string> mp= {
+    {"zero", "x0"},
+    {"ra", "x1"},
+    {"sp", "x2"},
+    {"gp", "x3"},
+    {"tp", "x4"},
+    {"t0", "x5"},
+    {"t1", "x6"},
+    {"t2", "x7"},
+    {"s0", "x8"},
+    {"s1", "x9"},
+    {"a0", "x10"},
+    {"a1", "x11"},
+    {"a2", "x12"},
+    {"a3", "x13"},
+    {"a4", "x14"},
+    {"a5", "x15"},
+    {"a6", "x16"},
+    {"a7", "x17"},
+    {"s2", "x18"},
+    {"s3", "x19"},
+    {"s4", "x20"},
+    {"s5", "x21"},
+    {"s6", "x22"},
+    {"s7", "x23"},
+    {"s8", "x24"},
+};
+
 string convert_to_hex(string a){
     bitset<32> b(a);
     unsigned long int x=b.to_ulong();
