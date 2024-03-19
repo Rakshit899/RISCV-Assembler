b9835b6d8cea6befe9f094b6114bb99a3414b2ff
Rakshit899 <2022csb1110@iitrpr.ac.in>
2024-03-19 22:20:15 +0530

    branch

--- branch_prediction/code.cpp
+++ branch_prediction/code.cpp
@@ -1,15 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-// vector<int> single,two,all_taken,all_not;
-map<int, pair<bool,bool>>two_bit_map;
-unordered_map<int,bool> single_bit_map;
+bool last_taken=true;
 int num_single=0,num_double=0,num_all_taken=0,num_all_not=0;
-void single_bit(int pc,bool taken){
-    if(taken==single_bit_map[pc]){
+void single_bit(bool taken){
+    if(taken==last_taken){
         num_single++;
     }
-    else{
-        single_bit_map[pc]=taken;
+    last_taken=taken;
+}
+void always_taken(bool taken){
+    if(taken==true){
+        num_all_taken++;
+    }
+}
+void always_not_taken(bool taken){
+    if(taken==false){
+        num_all_not++;
     }
 }
 int main(){
