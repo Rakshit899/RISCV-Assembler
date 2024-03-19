c3686e6390da9e52f6e41e4dd3e7c4495be8e6b1
Mudit-iitropar <149925616+Mudit-iitropar@users.noreply.github.com>
2024-03-19 22:24:39 +0530

    Update code.cpp

--- branch_prediction/code.cpp
+++ branch_prediction/code.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool last_taken=true;
+bool last_taken=false;
+bool two_bit_current=false, two_bit_prev=false;
 int num_single=0,num_double=0,num_all_taken=0,num_all_not=0;
 void single_bit(bool taken){
     if(taken==last_taken){
@@ -18,6 +19,25 @@ void always_not_taken(bool taken){
         num_all_not++;
     }
 }
+
+void two_bit( bool taken){
+    bool predict;
+    if(two_bit_prev == two_bit_current ){
+        predict = two_bit_prev;
+        two_bit_prev=taken;
+    }
+    else{
+        predict = two_bit_current;
+        if(taken == two_bit_prev){
+            two_bit_current= taken;
+        }
+        else{
+            two_bit_prev= taken;
+        }
+    }
+    if(predict == taken) num_double++;
+}
+
 int main(){
 
     return 0;
