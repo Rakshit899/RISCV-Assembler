1e9841813291d1fe41807f7975d4ca262f967a57
Mudit-iitropar <149925616+Mudit-iitropar@users.noreply.github.com>
2024-03-20 21:54:44 +0530

    Update code.cpp

--- branch_prediction/code.cpp
+++ branch_prediction/code.cpp
@@ -39,6 +39,69 @@ void two_bit( bool taken){
 }
 
 int main(){
+    string ifilename1 = "actual_pc.txt";
+    string ifilename2 = "machine_code.txt";
 
-    return 0;
+    ifstream ifile1(ifilename1);
+    ifstream ifile2(ifilename2);
+
+    if (!ifile1.is_open() || !ifile2.is_open()) {
+        cout << "Error opening files." << endl;
+        return 1;
+    }
+
+    vector<int> pc;
+
+    string line1;
+    while (getline(ifile1, line1)) {
+        if(line1.size() == 0) continue;
+        int value = stoi(line1, nullptr, 2);
+        pc.push_back(value);
+    }
+
+    string line2;
+
+    vector<int> target;
+
+    while (getline(ifile2, line2)) {
+        if(line2.size() == 0) continue;
+        string opcode = line2.substr(32-7, 7);
+        if(opcode == "1100011"){
+            string imm(13, '0');
+            imm[12-12] = line2[31-31];
+            imm[12-11] = line2[31-7];
+            for(int i = 31-25, j = 12-5; i >= 31-30, j >= 12-10; i--, j--){
+                imm[j] = line2[i];
+            }
+            for(int i = 31-8, j = 12-1; i >= 31-11, j >= 12-4; i--, j--){
+                imm[j] = line2[i];
+            }
+            int value = stoi(imm, nullptr, 2);
+            if(imm[0]=='1'){
+                value= value - pow(2,13);
+            }
+            target.push_back(value);
+
+        } else {
+            target.push_back(0);
+        }
+    }
+
+    vector<bool> choice;
+
+    for(int i = 0; i < pc.size()-1; i++){
+        if(pc[i+1]-pc[i] == target[i]){
+            choice.push_back(1);
+        } else {
+            choice.push_back(0);
+        }
+    }
+
+    for(auto x : choice){
+        cout << x << " ";
+    }
+
+    ifile1.close();
+    ifile2.close();
+
 }
